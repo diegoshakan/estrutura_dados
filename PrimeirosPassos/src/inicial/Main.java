57bd8d7f5c6f19fdbd1890fe3e3471b59c72e81d
Diego Araujo <diego_shakan@hotmail.com>
2018-08-02 09:01:10 -0300

    Primeiros Passos no mundo Java

---
+++ PrimeirosPassos/src/inicial/Main.java
@@ -0,0 +1,9 @@
+package inicial;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hey, Brother!");
+        System.out.println("Hello World!!");
+        
+    }
+    
+}
